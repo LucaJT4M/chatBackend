27a582a29c1a90800e2842552494d0a3bc84a512
KennyKT4M <kenny.kleeberg@trans4mation.de>
2026-01-16 12:51:38 +0100

    show_chat hinzugefügt

--- API.py
+++ API.py
@@ -7,17 +7,17 @@ app = FastAPI()
 def create_user(username: str, password: str):
     
 
-@app.get ("/chat")
-def chat():
-    # hier soll auf die Datenbank zugegriffen werden und die User ID's gezogen werden
-    return "Chat wurde erstellt"
+@app.get ("/show_chat/{chat_id}")
+def show_chat(chat_id: int):
+    # Chat anzeigen lassen
+    return "user1_id, user2_id, chat_id"
 
 @app.get("/users")
 def users(): 
     return "Hallo"
 
 @app.post("/send_message/{username1},{username2},{message}")
-async def send_message(user1_ID: int, user2_ID: int, message: str):
+async def send_message(user1_id: int, user2_id: int, message: str):
     # Input: user1_Id, user2_id, message
     # User sendet Nachricht an anderen User. Wenn Chat noch nicht erstellt, dann einen erstellen. Wenn einer schon
     # erstellt ist, passiert nix und nachricht wird in den chat geschickt
