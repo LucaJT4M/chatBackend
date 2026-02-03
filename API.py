839ec7ea69fc266869df84526c40fc65215dcf0f
KennyKT4M <kenny.kleeberg@trans4mation.de>
2026-02-03 15:08:24 +0100

    adding "get_chats_from_users"

--- API.py
+++ API.py
@@ -5,14 +5,20 @@ app = FastAPI()
 
 @app.post("/create_user/{username},{password}")
 def add_user(username: str, password: str):
+    response = {
+        "created": False,
+        "msg": "User konnte nicht erstellt werden.",
+        "exception": ""
+    }
     try:
-        
-
-
-
-
-
-    return create_user(username, password)
+        user = create_user(username, password)
+        create_user(username, password)
+        response["created"] = True
+        response["msg"] = "User wurde erstellt."
+    except Exception as e: 
+        response["msg"] = "User konnte nicht erstellt werden."
+        response["exception"] = e
+    return response
 
 @app.delete("/delete_user/{user_id}")
 def del_user(user_id: int):
@@ -30,7 +36,6 @@ def show_chat(chat_id: int):
         if c.id == chat_id: # Checkt jede Chat ID aus der DB und vergleich sie mit der zu suchenden ID
             return_chat = c # Weist der return_chat variable dem aktuellen Chat zu 
             break
-
     return return_chat
 
 @app.get("/users")
@@ -55,7 +60,6 @@ def login(username: str, password: str):
             response["msg"] = "Passwort falsch, bitte erneut versuchen."
     else:
         response["msg"] = "User wurde nicht gefunden."
-
     return response
 
 @app.post("/send_message/{sendername},{chat_id},{message}")
@@ -76,9 +80,24 @@ async def send_message(sendername: str, chat_id: int, message: str):
     except Exception as e: 
         response["msg"] = "Nachricht konnte nicht gesendet werden, bitte erneut versuchen."
         response["exception"] = e
-   
     return response
+
+@app.get("/get_chats_from_users/{username}")
+def get_chats_from_users(username: str):
+    user = get_user_by_name(username)
+    chats = get_chats()
+    user_chats = []
+    for c in chats: # for schleife, die durch jeden Chat durch geht und weist das aktuelle Chatobjekt der Variable "c" zu
+        if c.user1_id == user.id or c.user2_id == user.id: # Prüft, ob angegebener User in dem Chat mit drin ist
+            user_chats.append(c) # Wenn ja, wird der Liste das aktuelle Objekt "c" hinzugefügt.
+    return user_chats
+
+
+
+
+
 
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app)
+
